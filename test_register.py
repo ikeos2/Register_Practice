bd2cb0da8465ef4878b1bdb40e92b90778224d8f
Alex! <ikeos2@gmail.com>
2019-04-22 22:16:13 -0500

    Failing test, Implementing discounts

--- test_register.py
+++ test_register.py
@@ -18,7 +18,9 @@ class TestRegister(TestCase):
     def test_scan_unknown_item(self):
         self.assertEqual(self.reg.scan_item("Mystery Goo", 10), 0.00)
 
-    # def test_scan_multiple_items(self):
+    def test_scan_multiple_items(self):
+        self.assertEqual(self.reg.scan_item("Milk", 1), 4.99)
+        self.assertEqual(self.reg.scan_item("Ground Beef", 1), 3.99)
 
     def test_get_initial_total(self):
         self.assertEqual(self.reg.get_total(), 0.00)
@@ -26,4 +28,14 @@ class TestRegister(TestCase):
     def test_get_total_with_item(self):
         self.reg.scan_item("Milk", 1)
         self.assertEqual(self.reg.get_total(), 4.99)
+
+    def test_scan_and_total_multiple_items(self):
+        self.assertEqual(self.reg.scan_item("Milk", 1), 4.99)
+        self.assertEqual(self.reg.scan_item("Ground Beef", 1), 3.99)
+        self.assertEqual(self.reg.get_total(), 8.98)
+
+    def test_gum_2_for_1(self):
+        self.reg.scan_item("Gum", 2)
+        self.assertEqual(self.reg.get_total(), .25)
+
 
