470ad7944d947f3075e6c6761fad30ecbe8a1fd8
Alex! <ikeos2@gmail.com>
2019-04-22 22:09:06 -0500

    Added more items for testing

--- priceRepository.py
+++ priceRepository.py
@@ -1,3 +1,7 @@
 # Item: per unit price
 priceRepository = {"Ground Beef": 3.99,
-                   "Milk": 4.99}
+                   "Milk": 4.99,
+                   "Eggs": 1.37,
+                   "Bread": 3.50,
+                   "Gum": .25,
+                   "Round up for charity": .01}
