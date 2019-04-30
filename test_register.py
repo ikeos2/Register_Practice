596a8799e2476580d5c4ed9640b774db0410091b
Alex! <ikeos2@gmail.com>
2019-04-30 18:21:46 -0500

    Refactor MORE tests, arguments in wrong order

--- test_register.py
+++ test_register.py
@@ -8,56 +8,67 @@ class TestRegister(TestCase):
         self.reg = Register()
 
     def test_initial_register_state(self):
-        self.assertEqual(self.reg.get_total(), 0.00)
+        results = 0.00
+        self.assertEqual(results, self.reg.get_total())
 
     def test_scan_item_ground_beef(self):
-        self.assertEqual(self.reg.scan_item("Ground Beef", 1), 3.99)
+        results = 3.99
+        self.assertEqual(results, self.reg.scan_item("Ground Beef", 1))
 
     def test_scan_item_milk(self):
-        self.assertEqual(self.reg.scan_item("Milk", 1), 4.99)
+        results = 4.99
+        self.assertEqual(results, self.reg.scan_item("Milk", 1))
 
     def test_scan_unknown_item(self):
-        self.assertEqual(self.reg.scan_item("Mystery Goo", 10), 0.00)
+        results =  0.00
+        self.assertEqual(results, self.reg.scan_item("Mystery Goo", 10))
 
     def test_scan_multiple_items(self):
-        self.assertEqual(self.reg.scan_item("Milk", 1), 4.99)
-        self.assertEqual(self.reg.scan_item("Ground Beef", 1), 3.99)
+        self.assertEqual(4.99, self.reg.scan_item("Milk", 1))
+        self.assertEqual(3.99, self.reg.scan_item("Ground Beef", 1))
 
     def test_get_initial_total(self):
-        self.assertEqual(self.reg.get_total(), 0.00)
+        results = 0.00
+        self.assertEqual(results, self.reg.get_total())
 
     def test_get_total_with_item(self):
         self.reg.scan_item("Milk", 1)
-        self.assertEqual(self.reg.get_total(), 4.99)
+        results = 4.99
+        self.assertEqual(results, self.reg.get_total())
 
     def test_scan_and_total_multiple_items(self):
-        self.assertEqual(self.reg.scan_item("Milk", 1), 4.99)
-        self.assertEqual(self.reg.scan_item("Ground Beef", 1), 3.99)
-        self.assertEqual(self.reg.get_total(), 8.98)
+        self.assertEqual(4.99, self.reg.scan_item("Milk", 1))
+        self.assertEqual(3.99, self.reg.scan_item("Ground Beef", 1))
+        self.assertEqual(8.98, self.reg.get_total())
 
     def test_gum_2_for_1(self):
         self.reg.scan_item("Gum", 2)
-        self.assertEqual(self.reg.get_total(), .25)
+        results = .25
+        self.assertEqual(results, self.reg.get_total())
 
     def test_void_an_item(self):
         self.reg.scan_item("Milk", 1)
         self.reg.void_item("Milk", 1)
-        self.assertEqual(self.reg.get_total(), 0.00)
+        result = 0.00
+        self.assertEqual(result, self.reg.get_total())
 
     def test_void_an_item_with_one_leftover(self):
         self.reg.scan_item("Milk", 2)
         self.reg.void_item("Milk", 1)
-        self.assertEqual(self.reg.get_total(), 4.99)
+        results = 4.99
+        self.assertEqual(results, self.reg.get_total())
 
     def test_total_is_calculated_right_after_removing_special_item1(self):
         self.reg.scan_item("Gum", 3)
         self.reg.void_item("Gum", 1)
-        self.assertEqual(self.reg.get_total(), .25)
+        results = .25
+        self.assertEqual(results, self.reg.get_total())
 
     def test_total_is_calculated_right_after_removing_special_item2(self):
         self.reg.scan_item("Gum", 3)
         self.reg.void_item("Gum", 2)
-        self.assertEqual(self.reg.get_total(), .25)
+        results = .25
+        self.assertEqual(results,  self.reg.get_total())
 
 
 if __name__ == '__main__':
